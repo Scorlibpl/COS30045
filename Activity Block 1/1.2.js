03bf48fb6094d0f46d145022ee4dfe8978f51554
Scorlib Perjuangan L <scorlibpl@gmail.com>
2021-03-24 21:46:44 +0700

    Add files via upload

---
+++ Activity Block 1/1.2.js
@@ -0,0 +1,9 @@
+function myFunction1(){
+    document.getElementById("img1").src = "images/NT_Naplan_Reading_Results_2011.png";
+    document.getElementById("fig").innerHTML = "Fig 1. Percent of children above national minimum standard in reading for 2011 Year 3, 5, 7 and 9 for Non-Indigenous and Indigenous children in the Northen Territory. Data Source: " + "<a href='https://channels.theinnovationenterprise.com/articles/the-future-of-data-visualization' target='_blank'><strong>NAPLAN results</strong></a>";
+}
+
+function myFunction2(){
+    document.getElementById("img1").src = "images/NT_Naplan_Reading_Results_2017.png";
+    document.getElementById("fig").innerHTML = "Fig 2. Percent of children above national minimum standard in reading for 2017 Year 3, 5, 7 and 9 for Non-Indigenous and Indigenous children in the Northen Territory. Data Source: " + "<a href='https://channels.theinnovationenterprise.com/articles/the-future-of-data-visualization' target='_blank'><strong>NAPLAN results</strong></a>";
+}
